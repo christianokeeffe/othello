8f2b94575ddb4834092af8bfd41e6dfc6f5b53d5
Kasper Plejdrup <kplejd11@student.aau.dk>
2014-10-22 13:17:18 +0200

    Added Person class

---
+++ OthelloProject/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OthelloProject
+{
+    class Person
+    {
+        private string _name;
+        private List<string> _friends;
+        private string _summary;
+        private double _review;
+    
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        public List<string> Friends
+        {
+            get { return _friends; }
+            set { _friends = value; }
+        }
+
+    }
+}
