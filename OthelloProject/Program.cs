1eb93877b9500ffe04d3c3627f333ec7292d4cb9
Christian OKeeffe <christian@joedal.dk>
2014-11-05 11:13:04 +0100

    started prob

--- OthelloProject/Program.cs
+++ OthelloProject/Program.cs
@@ -103,7 +103,7 @@ namespace OthelloProject
             //Chr Path
             List<Person> PersonList = Readfile.insertPeople("C:\\Users\\Christian\\Downloads\\friendships.txt");
             //List<Person> PersonList = Readfile.insertPeople("C:\\Users\\Christian\\Dropbox\\Documents\\Arbejde\\UNI\\P7\\Undervisning\\Web Intelligence\\ChrMetKas\\testfriendships.txt");
-            cluster.splintNumbTimes(OurMatrix.createMatrix(PersonList),4);
+            cluster.splintNumbTimes(OurMatrix.createMatrix(PersonList),2);
             //Kasper Test path
             //List<Person> PersonList = Readfile.insertPeople("C:\\test.txt");
             //List<Review> ReviewList = Readfile.loadReviews("C:\\Users\\Kasper\\Documents\\Uni\\P7\\Undervisning\\Web Intelligence\\ChrMetKas\\testreviews.txt");
---
+++ OthelloProject/prob.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OthelloProject
+{
+    class prob
+    {
+        List<Review> listOfReviews;
+        List<List<Review>> GroupedListOfReviews = new List<List<Review>>();
+        List<term> termList = new List<term>();
+        public prob (List<Review> listOfReviews)
+        {
+            this.listOfReviews = listOfReviews;
+            for(int i = 0; i < 5; i++)
+            {
+                GroupedListOfReviews.Add(new List<Review>());
+            }
+            for(int i = 0; i < listOfReviews.Count; i++)
+            {
+                GroupedListOfReviews[(int)listOfReviews[i].Score].Add(listOfReviews[i]);
+            }
+        }
+
+        public double getPofClass(int classnumb)
+        {
+            return (GroupedListOfReviews[classnumb].Count + 1) / (listOfReviews.Count + GroupedListOfReviews.Count);
+        }
+
+        public double getProbOfTermInClass(string term, int classnumb)
+        {
+            return termList.Find(x => x.termName == term).termProb[classnumb];
+        }
+
+        private int getClassOfReview(Review rev)
+        {
+            List<double> scores = new List<double>();
+            for(int i = 0; i < 5; i++)
+            {
+
+            }
+        }
+    }
+}
